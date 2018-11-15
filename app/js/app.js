91e1c25c6df0a10b7f29bfd6efd6e9691833d32d
tanvir haider <tanvir@willow.studio>
2018-11-15 10:51:05 -0500

    new

--- app/js/app.js
+++ app/js/app.js
@@ -48,9 +48,6 @@ function initInfoAnimation (d) {
 
     //  
 
-
-
-
     var title_H2 = new Sprite({
         id:'title_H2', 
         class: 'title_H2-style', 
@@ -67,278 +64,291 @@ function initInfoAnimation (d) {
 
     // module 1 -----------------------
 
-    var mod1 = new Sprite({
-        id:'mod1', 
-        class: 'mod1-style',
-        container:mod.obj
-    });
-
-    var mod1lines = new Sprite({
-        id:'mod1lines', 
-        class: 'mod1lines-style',
-        container:mod1.obj
-    });
-
-    for (var i = 0; i < 4; i++) {
-        var lineItems = new Sprite({
-            id:'lineItems-' + i, 
-            class: 'lineItems-style',
-            container:mod1lines.obj
+    function startMod1 () {
+        var mod1 = new Sprite({
+            id:'mod1', 
+            class: 'mod1-style',
+            container:mod.obj
         });
-
-        var lineItemvalue = new Sprite({
-            id:'lineItemvalue-' + i, 
-            class: 'lineItemvalue-style',
-            text: {content: verticalValue[i]},
-            container:lineItems.obj
+    
+        var mod1lines = new Sprite({
+            id:'mod1lines', 
+            class: 'mod1lines-style',
+            container:mod1.obj
         });
-
-        var lineItembg = new Sprite({
-            id:'lineItembg-' + i, 
-            class: 'lineItembg-style',
-            container:lineItems.obj
+    
+        for (var i = 0; i < 4; i++) {
+            var lineItems = new Sprite({
+                id:'lineItems-' + i, 
+                class: 'lineItems-style',
+                container:mod1lines.obj
+            });
+    
+            var lineItemvalue = new Sprite({
+                id:'lineItemvalue-' + i, 
+                class: 'lineItemvalue-style',
+                text: {content: verticalValue[i]},
+                container:lineItems.obj
+            });
+    
+            var lineItembg = new Sprite({
+                id:'lineItembg-' + i, 
+                class: 'lineItembg-style',
+                container:lineItems.obj
+            });
+        }
+    
+        var mod2lines = new Sprite({
+            id:'mod2lines', 
+            class: 'mod2lines-style',
+            container:mod1.obj
         });
+    
+        for (var i = 0; i < 12; i++ ) {
+            var lineItems2 = new Sprite({
+                id:'lineItems2-' + i, 
+                class: 'lineItems2-style',
+                container:mod2lines.obj
+            });
+    
+            var linesContainer = new Sprite({
+                id:'linesContainer-' + i, 
+                class: 'linesContainer-style',
+                container:lineItems2.obj
+            });
+    
+            var lineItemleft = new Sprite({
+                id:'lineItemleft-' + i, 
+                class: 'lineItemleft-style',
+                container:linesContainer.obj
+            });
+    
+            var lineItemright = new Sprite({
+                id:'lineItemright-' + i, 
+                class: 'lineItemright-style',
+                container:linesContainer.obj
+            });
+    
+            var lineItemleftbg = new Sprite({
+                id:'lineItemleftbg-' + i, 
+                class: 'lineItemleftbg-style',
+                container:lineItemleft.obj
+            });
+    
+            var lineItemrightbg = new Sprite({
+                id:'lineItemrightbg-' + i, 
+                class: 'lineItemrightbg-style',
+                container:lineItemright.obj
+            });
+    
+            lineItemleft.obj.style.height = yearLeftValue[i] + "%";
+            lineItemright.obj.style.height = yearRightValue[i] + "%";
+    
+    
+            TweenMax.fromTo(lineItemleftbg.obj, 1, {height:"0%"},{ease: Power2.easeInOut, delay:i*0.1,height:"100%"});
+            TweenMax.fromTo(lineItemrightbg.obj, 1, {height:"0%"},{ease: Power2.easeInOut, delay:i*0.1,height:"100%"});
+    
+            var yearsContainer = new Sprite({
+                id:'yearsContainer-' + i, 
+                class: 'yearsContainer-style',
+                container:lineItems2.obj,
+                text: {content: yearInformation[i]}
+            });
+    
+        }
     }
 
-    var mod2lines = new Sprite({
-        id:'mod2lines', 
-        class: 'mod2lines-style',
-        container:mod1.obj
-    });
+    startMod1 ();
 
-    for (var i = 0; i < 12; i++ ) {
-        var lineItems2 = new Sprite({
-            id:'lineItems2-' + i, 
-            class: 'lineItems2-style',
-            container:mod2lines.obj
-        });
-
-        var linesContainer = new Sprite({
-            id:'linesContainer-' + i, 
-            class: 'linesContainer-style',
-            container:lineItems2.obj
-        });
-
-        var lineItemleft = new Sprite({
-            id:'lineItemleft-' + i, 
-            class: 'lineItemleft-style',
-            container:linesContainer.obj
-        });
-
-        var lineItemright = new Sprite({
-            id:'lineItemright-' + i, 
-            class: 'lineItemright-style',
-            container:linesContainer.obj
-        });
-
-        var lineItemleftbg = new Sprite({
-            id:'lineItemleftbg-' + i, 
-            class: 'lineItemleftbg-style',
-            container:lineItemleft.obj
-        });
-
-        var lineItemrightbg = new Sprite({
-            id:'lineItemrightbg-' + i, 
-            class: 'lineItemrightbg-style',
-            container:lineItemright.obj
-        });
-
-        lineItemleft.obj.style.height = yearLeftValue[i] + "%";
-        lineItemright.obj.style.height = yearRightValue[i] + "%";
-
-
-        TweenMax.fromTo(lineItemleftbg.obj, 1, {height:"0%"},{ease: Power2.easeInOut, delay:i*0.1,height:"100%"});
-        TweenMax.fromTo(lineItemrightbg.obj, 1, {height:"0%"},{ease: Power2.easeInOut, delay:i*0.1,height:"100%"});
-
-        var yearsContainer = new Sprite({
-            id:'yearsContainer-' + i, 
-            class: 'yearsContainer-style',
-            container:lineItems2.obj,
-            text: {content: yearInformation[i]}
-        });
-
-    }
+   
 
     // end of Module 1 -------------
 
 
 
     // module 2 -----------------------------------
-    var mod2 = new Sprite({
-        id:'mod2', 
-        class: 'mod2-style',
-        container:mod.obj
-    });
 
-    var mod2copy1 = new Sprite({
-        id:'mod2copy1', 
-        class: 'mod2copy1-style', 
-        container:mod2.obj, 
-        text: {content: d.header2}
-    });
+    function startMod2 () {
+        var mod2 = new Sprite({
+            id:'mod2', 
+            class: 'mod2-style',
+            container:mod.obj
+        });
+    
+        var mod2copy1 = new Sprite({
+            id:'mod2copy1', 
+            class: 'mod2copy1-style', 
+            container:mod2.obj, 
+            text: {content: d.header2}
+        });
+    
+       
+    
+        var mod2graphics1 = new Sprite({
+            id:'mod2graphics1', 
+            class: 'mod2graphics1-style',
+            container:mod2.obj,
+            image: "assets/images/mod2graphix1@2x.png"
+        });
+    
+       
+    
+        var mod2copy2 = new Sprite({
+            id:'mod2copy2', 
+            class: 'mod2copy2-style', 
+            container:mod2.obj, 
+            text: {content: d.subheader2}
+        });
+    
+    
+       
+    
+        
+    
+        var mod2graphics2 = new Sprite({
+            id:'mod2graphics2', 
+            class: 'mod2graphics2-style',
+            container:mod2.obj
+        });
+    
+    
+    
+        
+    
+        var m2b1 = new Sprite({
+            id:'m2b1', 
+            class: 'm2b1-style',
+            container:mod2graphics2.obj
+        });
+    
+    
+       
+    
+        var m2b1_bg = new Sprite({
+            id:'m2b1_bg', 
+            class: 'm2b1_bg-style',
+            container:m2b1.obj
+        });
+    
+        var m2b1_country = new Sprite({
+            id:'m2b1_country', 
+            class: 'm2b1_country-style',
+            container:m2b1_bg.obj,
+            text: {content: d.usa}
+        });
+    
+        var m2b1_value = new Sprite({
+            id:'m2b1_value', 
+            class: 'm2b1_value-style',
+            container:m2b1_bg.obj,
+            text: {content: '22%'}
+        });
+    
+        var m2b2 = new Sprite({
+            id:'m2b2', 
+            class: 'm2b2-style',
+            container:mod2graphics2.obj
+        });
+    
+       
+    
+        var m2b2_bg = new Sprite({
+            id:'m2b2_bg', 
+            class: 'm2b2_bg-style',
+            container:m2b2.obj
+        });
+    
+        var m2b2_country = new Sprite({
+            id:'m2b2_country', 
+            class: 'm2b2_country-style',
+            container:m2b2_bg.obj,
+            text: {content: d.germany}
+        });
+    
+        var m2b2_value = new Sprite({
+            id:'m2b2_value', 
+            class: 'm2b2_value-style',
+            container:m2b2_bg.obj
+         //   text: {content: '31%'}
+        });
+    
+        var m2b3 = new Sprite({
+            id:'m2b3', 
+            class: 'm2b3-style',
+            container:mod2graphics2.obj
+        });
+    
+    
+        var m2b3_bg = new Sprite({
+            id:'m2b3_bg', 
+            class: 'm2b3_bg-style',
+            container:m2b3.obj
+        });
+    
+        var m2b3_country = new Sprite({
+            id:'m2b3_country', 
+            class: 'm2b3_country-style',
+            container:m2b3_bg.obj,
+            text: {content: d.china}
+        });
+    
+        var m2b3_value = new Sprite({
+            id:'m2b3_value', 
+            class: 'm2b3_value-style',
+            container:m2b3_bg.obj,
+            text: {content: '79%'}
+        });
+    
+        var game = {
+            score1:0,
+            score2:0,
+            score3:0
+        };
+    
+    
+        TweenMax.fromTo(mod2copy1.obj, 1, {alpha:0},{delay: 0.5, ease: Power1.easeInOut, alpha:1});
+        TweenMax.fromTo(mod2graphics1.obj, 1, {alpha:0},{delay: 1, ease: Power1.easeInOut, alpha:1});
+        TweenMax.fromTo(mod2copy2.obj, 1, {alpha:0},{delay: 1.2, ease: Power1.easeInOut, alpha:1});
+        TweenMax.fromTo(m2b1_bg.obj, 1, {width:"0%"},{delay: 1, ease: Power1.easeInOut, width:"33%"});
+        TweenMax.fromTo(m2b2_bg.obj, 1, {width:"0%"},{delay: 1.5, ease: Power1.easeInOut, width:"43%"});
+        TweenMax.fromTo(m2b3_bg.obj, 1, {width:"0%"},{delay: 2, ease: Power1.easeInOut, width:"100%"});
+        TweenMax.to(game, 1, {delay: 1,score1:"+=22", roundProps:"score1", onUpdate:function() {document.getElementById("m2b1_value").innerText = game.score1 + "%";}, ease:Power1.easeInOut});
+        TweenMax.to(game, 1, {delay: 1.5,score2:"+=31", roundProps:"score2", onUpdate:function() {document.getElementById("m2b2_value").innerText = game.score2 + "%";}, ease:Power1.easeInOut});
+        TweenMax.to(game, 1, {delay: 2,score3:"+=79", roundProps:"score3", onUpdate:function() {document.getElementById("m2b3_value").innerText = game.score3 + "%";}, ease:Power1.easeInOut});
+    }
 
-    TweenMax.fromTo(mod2copy1.obj, 1, {alpha:0},{delay: 0.5, ease: Power1.easeInOut, alpha:1});
-
-    var mod2graphics1 = new Sprite({
-        id:'mod2graphics1', 
-        class: 'mod2graphics1-style',
-        container:mod2.obj,
-        image: "assets/images/mod2graphix1@2x.png"
-    });
-
-    TweenMax.fromTo(mod2graphics1.obj, 1, {alpha:0},{delay: 1, ease: Power1.easeInOut, alpha:1});
-
-    var mod2copy2 = new Sprite({
-        id:'mod2copy2', 
-        class: 'mod2copy2-style', 
-        container:mod2.obj, 
-        text: {content: d.subheader2}
-    });
-
-    TweenMax.fromTo(mod2copy2.obj, 1, {alpha:0},{delay: 1.2, ease: Power1.easeInOut, alpha:1});
-
-    var mod2graphics2 = new Sprite({
-        id:'mod2graphics2', 
-        class: 'mod2graphics2-style',
-        container:mod2.obj
-    });
-
+    startMod2 ();
     
 
-    var m2b1 = new Sprite({
-        id:'m2b1', 
-        class: 'm2b1-style',
-        container:mod2graphics2.obj
-    });
+    function startMod3 () {
+        var mod3 = new Sprite({
+            id:'mod3', 
+            class: 'mod3-style',
+            container:mod.obj
+        });
+    
+    
+        var mod3copy1 = new Sprite({
+            id:'mod3copy1', 
+            class: 'mod3copy1-style', 
+            container:mod3.obj, 
+            text: {content: d.header3}
+        });
+    
+        var mod3graphics1 = new Sprite({
+            id:'mod3graphics1', 
+            class: 'mod3graphics1-style',
+            container:mod3.obj,
+            image: "assets/images/mod3graphix1@2x.png"
+        });
+    
+        TweenMax.fromTo(mod3copy1.obj, 1, {alpha:0},{delay: 1.5, ease: Power1.easeInOut, alpha:1});
+        TweenMax.fromTo(mod3graphics1.obj, 1, {alpha:0},{delay: 2, ease: Power1.easeInOut, alpha:1});
+    
+        console.log('version 1');
+    }
 
-
-   
-
-    var m2b1_bg = new Sprite({
-        id:'m2b1_bg', 
-        class: 'm2b1_bg-style',
-        container:m2b1.obj
-    });
-
-    var m2b1_country = new Sprite({
-        id:'m2b1_country', 
-        class: 'm2b1_country-style',
-        container:m2b1_bg.obj,
-        text: {content: d.usa}
-    });
-
-    var m2b1_value = new Sprite({
-        id:'m2b1_value', 
-        class: 'm2b1_value-style',
-        container:m2b1_bg.obj,
-        text: {content: '22%'}
-    });
-
-    var m2b2 = new Sprite({
-        id:'m2b2', 
-        class: 'm2b2-style',
-        container:mod2graphics2.obj
-    });
-
-   
-
-    var m2b2_bg = new Sprite({
-        id:'m2b2_bg', 
-        class: 'm2b2_bg-style',
-        container:m2b2.obj
-    });
-
-    var m2b2_country = new Sprite({
-        id:'m2b2_country', 
-        class: 'm2b2_country-style',
-        container:m2b2_bg.obj,
-        text: {content: d.germany}
-    });
-
-    var m2b2_value = new Sprite({
-        id:'m2b2_value', 
-        class: 'm2b2_value-style',
-        container:m2b2_bg.obj
-     //   text: {content: '31%'}
-    });
-
-    var m2b3 = new Sprite({
-        id:'m2b3', 
-        class: 'm2b3-style',
-        container:mod2graphics2.obj
-    });
-
-
-    var m2b3_bg = new Sprite({
-        id:'m2b3_bg', 
-        class: 'm2b3_bg-style',
-        container:m2b3.obj
-    });
-
-    var m2b3_country = new Sprite({
-        id:'m2b3_country', 
-        class: 'm2b3_country-style',
-        container:m2b3_bg.obj,
-        text: {content: d.china}
-    });
-
-    var m2b3_value = new Sprite({
-        id:'m2b3_value', 
-        class: 'm2b3_value-style',
-        container:m2b3_bg.obj,
-        text: {content: '79%'}
-    });
-
-  
-
-    TweenMax.fromTo(m2b1_bg.obj, 1, {width:"0%"},{delay: 1, ease: Power1.easeInOut, width:"33%"});
-    TweenMax.fromTo(m2b2_bg.obj, 1, {width:"0%"},{delay: 1.5, ease: Power1.easeInOut, width:"43%"});
-    TweenMax.fromTo(m2b3_bg.obj, 1, {width:"0%"},{delay: 2, ease: Power1.easeInOut, width:"100%"});
-
-
-    var game = {
-        score1:0,
-        score2:0,
-        score3:0
-    };
-
-    TweenMax.to(game, 1, {delay: 1,score1:"+=22", roundProps:"score1", onUpdate:function() {
-        document.getElementById("m2b1_value").innerText = game.score1 + "%";
-    }, ease:Power1.easeInOut});
-
-    TweenMax.to(game, 1, {delay: 1.5,score2:"+=31", roundProps:"score2", onUpdate:function() {
-        document.getElementById("m2b2_value").innerText = game.score2 + "%";
-    }, ease:Power1.easeInOut});
-
-    TweenMax.to(game, 1, {delay: 2,score3:"+=79", roundProps:"score3", onUpdate:function() {
-        document.getElementById("m2b3_value").innerText = game.score3 + "%";
-    }, ease:Power1.easeInOut});
-
-    var mod3 = new Sprite({
-        id:'mod3', 
-        class: 'mod3-style',
-        container:mod.obj
-    });
-
-
-    var mod3copy1 = new Sprite({
-        id:'mod3copy1', 
-        class: 'mod3copy1-style', 
-        container:mod3.obj, 
-        text: {content: d.header3}
-    });
-
-    var mod3graphics1 = new Sprite({
-        id:'mod3graphics1', 
-        class: 'mod3graphics1-style',
-        container:mod3.obj,
-        image: "assets/images/mod3graphix1@2x.png"
-    });
-
-    TweenMax.fromTo(mod3copy1.obj, 1, {alpha:0},{delay: 1.5, ease: Power1.easeInOut, alpha:1});
-    TweenMax.fromTo(mod3graphics1.obj, 1, {alpha:0},{delay: 2, ease: Power1.easeInOut, alpha:1});
-
-    console.log('version 1');
+    startMod3 ();
 
 }
 
@@ -351,4 +361,14 @@ function startInfographic (d) {
     initStage (d.container);
     initInfoAnimation(d.data);
 }
+
+
+
+function loadInfographic (d) {
+    
+    initStage (d.container);
+   // initInfoAnimation(d.data);
+}
+
+
 
--- build/js/app-min.js
+++ build/js/app-min.js
@@ -3883,236 +3883,227 @@ function initInfoAnimation(d) {
         }
       });
     }
-  }
-
-  startMod1(); // end of Module 1 -------------
+  } // startMod1 ();
+  // end of Module 1 -------------
   // module 2 -----------------------------------
 
-  function startMod2() {
-    var mod2 = new Sprite({
-      id: 'mod2',
-      class: 'mod2-style',
-      container: mod.obj
-    });
-    var mod2copy1 = new Sprite({
-      id: 'mod2copy1',
-      class: 'mod2copy1-style',
-      container: mod2.obj,
-      text: {
-        content: d.header2
-      }
-    });
-    var mod2graphics1 = new Sprite({
-      id: 'mod2graphics1',
-      class: 'mod2graphics1-style',
-      container: mod2.obj,
-      image: "assets/images/mod2graphix1@2x.png"
-    });
-    var mod2copy2 = new Sprite({
-      id: 'mod2copy2',
-      class: 'mod2copy2-style',
-      container: mod2.obj,
-      text: {
-        content: d.subheader2
-      }
-    });
-    var mod2graphics2 = new Sprite({
-      id: 'mod2graphics2',
-      class: 'mod2graphics2-style',
-      container: mod2.obj
-    });
-    var m2b1 = new Sprite({
-      id: 'm2b1',
-      class: 'm2b1-style',
-      container: mod2graphics2.obj
-    });
-    var m2b1_bg = new Sprite({
-      id: 'm2b1_bg',
-      class: 'm2b1_bg-style',
-      container: m2b1.obj
-    });
-    var m2b1_country = new Sprite({
-      id: 'm2b1_country',
-      class: 'm2b1_country-style',
-      container: m2b1_bg.obj,
-      text: {
-        content: d.usa
-      }
-    });
-    var m2b1_value = new Sprite({
-      id: 'm2b1_value',
-      class: 'm2b1_value-style',
-      container: m2b1_bg.obj,
-      text: {
-        content: '22%'
-      }
-    });
-    var m2b2 = new Sprite({
-      id: 'm2b2',
-      class: 'm2b2-style',
-      container: mod2graphics2.obj
-    });
-    var m2b2_bg = new Sprite({
-      id: 'm2b2_bg',
-      class: 'm2b2_bg-style',
-      container: m2b2.obj
-    });
-    var m2b2_country = new Sprite({
-      id: 'm2b2_country',
-      class: 'm2b2_country-style',
-      container: m2b2_bg.obj,
-      text: {
-        content: d.germany
-      }
-    });
-    var m2b2_value = new Sprite({
-      id: 'm2b2_value',
-      class: 'm2b2_value-style',
-      container: m2b2_bg.obj //   text: {content: '31%'}
 
-    });
-    var m2b3 = new Sprite({
-      id: 'm2b3',
-      class: 'm2b3-style',
-      container: mod2graphics2.obj
-    });
-    var m2b3_bg = new Sprite({
-      id: 'm2b3_bg',
-      class: 'm2b3_bg-style',
-      container: m2b3.obj
-    });
-    var m2b3_country = new Sprite({
-      id: 'm2b3_country',
-      class: 'm2b3_country-style',
-      container: m2b3_bg.obj,
-      text: {
-        content: d.china
-      }
-    });
-    var m2b3_value = new Sprite({
-      id: 'm2b3_value',
-      class: 'm2b3_value-style',
-      container: m2b3_bg.obj,
-      text: {
-        content: '79%'
-      }
-    });
-    var game = {
-      score1: 0,
-      score2: 0,
-      score3: 0
-    };
-    TweenMax.fromTo(mod2copy1.obj, 1, {
-      alpha: 0
-    }, {
-      delay: 0.5,
-      ease: Power1.easeInOut,
-      alpha: 1
-    });
-    TweenMax.fromTo(mod2graphics1.obj, 1, {
-      alpha: 0
-    }, {
-      delay: 1,
-      ease: Power1.easeInOut,
-      alpha: 1
-    });
-    TweenMax.fromTo(mod2copy2.obj, 1, {
-      alpha: 0
-    }, {
-      delay: 1.2,
-      ease: Power1.easeInOut,
-      alpha: 1
-    });
-    TweenMax.fromTo(m2b1_bg.obj, 1, {
-      width: "0%"
-    }, {
-      delay: 1,
-      ease: Power1.easeInOut,
-      width: "33%"
-    });
-    TweenMax.fromTo(m2b2_bg.obj, 1, {
-      width: "0%"
-    }, {
-      delay: 1.5,
-      ease: Power1.easeInOut,
-      width: "43%"
-    });
-    TweenMax.fromTo(m2b3_bg.obj, 1, {
-      width: "0%"
-    }, {
-      delay: 2,
-      ease: Power1.easeInOut,
-      width: "100%"
-    });
-    TweenMax.to(game, 1, {
-      delay: 1,
-      score1: "+=22",
-      roundProps: "score1",
-      onUpdate: function onUpdate() {
-        document.getElementById("m2b1_value").innerText = game.score1 + "%";
-      },
-      ease: Power1.easeInOut
-    });
-    TweenMax.to(game, 1, {
-      delay: 1.5,
-      score2: "+=31",
-      roundProps: "score2",
-      onUpdate: function onUpdate() {
-        document.getElementById("m2b2_value").innerText = game.score2 + "%";
-      },
-      ease: Power1.easeInOut
-    });
-    TweenMax.to(game, 1, {
-      delay: 2,
-      score3: "+=79",
-      roundProps: "score3",
-      onUpdate: function onUpdate() {
-        document.getElementById("m2b3_value").innerText = game.score3 + "%";
-      },
-      ease: Power1.easeInOut
-    });
-  }
+  var mod2 = new Sprite({
+    id: 'mod2',
+    class: 'mod2-style',
+    container: mod.obj
+  });
+  var mod2copy1 = new Sprite({
+    id: 'mod2copy1',
+    class: 'mod2copy1-style',
+    container: mod2.obj,
+    text: {
+      content: d.header2
+    }
+  });
+  TweenMax.fromTo(mod2copy1.obj, 1, {
+    alpha: 0
+  }, {
+    delay: 0.5,
+    ease: Power1.easeInOut,
+    alpha: 1
+  });
+  var mod2graphics1 = new Sprite({
+    id: 'mod2graphics1',
+    class: 'mod2graphics1-style',
+    container: mod2.obj,
+    image: "assets/images/mod2graphix1@2x.png"
+  });
+  TweenMax.fromTo(mod2graphics1.obj, 1, {
+    alpha: 0
+  }, {
+    delay: 1,
+    ease: Power1.easeInOut,
+    alpha: 1
+  });
+  var mod2copy2 = new Sprite({
+    id: 'mod2copy2',
+    class: 'mod2copy2-style',
+    container: mod2.obj,
+    text: {
+      content: d.subheader2
+    }
+  });
+  TweenMax.fromTo(mod2copy2.obj, 1, {
+    alpha: 0
+  }, {
+    delay: 1.2,
+    ease: Power1.easeInOut,
+    alpha: 1
+  });
+  var mod2graphics2 = new Sprite({
+    id: 'mod2graphics2',
+    class: 'mod2graphics2-style',
+    container: mod2.obj
+  });
+  var m2b1 = new Sprite({
+    id: 'm2b1',
+    class: 'm2b1-style',
+    container: mod2graphics2.obj
+  });
+  var m2b1_bg = new Sprite({
+    id: 'm2b1_bg',
+    class: 'm2b1_bg-style',
+    container: m2b1.obj
+  });
+  var m2b1_country = new Sprite({
+    id: 'm2b1_country',
+    class: 'm2b1_country-style',
+    container: m2b1_bg.obj,
+    text: {
+      content: d.usa
+    }
+  });
+  var m2b1_value = new Sprite({
+    id: 'm2b1_value',
+    class: 'm2b1_value-style',
+    container: m2b1_bg.obj,
+    text: {
+      content: '22%'
+    }
+  });
+  var m2b2 = new Sprite({
+    id: 'm2b2',
+    class: 'm2b2-style',
+    container: mod2graphics2.obj
+  });
+  var m2b2_bg = new Sprite({
+    id: 'm2b2_bg',
+    class: 'm2b2_bg-style',
+    container: m2b2.obj
+  });
+  var m2b2_country = new Sprite({
+    id: 'm2b2_country',
+    class: 'm2b2_country-style',
+    container: m2b2_bg.obj,
+    text: {
+      content: d.germany
+    }
+  });
+  var m2b2_value = new Sprite({
+    id: 'm2b2_value',
+    class: 'm2b2_value-style',
+    container: m2b2_bg.obj //   text: {content: '31%'}
 
-  startMod2();
-
-  function startMod3() {
-    var mod3 = new Sprite({
-      id: 'mod3',
-      class: 'mod3-style',
-      container: mod.obj
-    });
-    var mod3copy1 = new Sprite({
-      id: 'mod3copy1',
-      class: 'mod3copy1-style',
-      container: mod3.obj,
-      text: {
-        content: d.header3
-      }
-    });
-    var mod3graphics1 = new Sprite({
-      id: 'mod3graphics1',
-      class: 'mod3graphics1-style',
-      container: mod3.obj,
-      image: "assets/images/mod3graphix1@2x.png"
-    });
-    TweenMax.fromTo(mod3copy1.obj, 1, {
-      alpha: 0
-    }, {
-      delay: 1.5,
-      ease: Power1.easeInOut,
-      alpha: 1
-    });
-    TweenMax.fromTo(mod3graphics1.obj, 1, {
-      alpha: 0
-    }, {
-      delay: 2,
-      ease: Power1.easeInOut,
-      alpha: 1
-    });
-    console.log('version 1');
-  }
-
-  startMod3();
+  });
+  var m2b3 = new Sprite({
+    id: 'm2b3',
+    class: 'm2b3-style',
+    container: mod2graphics2.obj
+  });
+  var m2b3_bg = new Sprite({
+    id: 'm2b3_bg',
+    class: 'm2b3_bg-style',
+    container: m2b3.obj
+  });
+  var m2b3_country = new Sprite({
+    id: 'm2b3_country',
+    class: 'm2b3_country-style',
+    container: m2b3_bg.obj,
+    text: {
+      content: d.china
+    }
+  });
+  var m2b3_value = new Sprite({
+    id: 'm2b3_value',
+    class: 'm2b3_value-style',
+    container: m2b3_bg.obj,
+    text: {
+      content: '79%'
+    }
+  });
+  TweenMax.fromTo(m2b1_bg.obj, 1, {
+    width: "0%"
+  }, {
+    delay: 1,
+    ease: Power1.easeInOut,
+    width: "33%"
+  });
+  TweenMax.fromTo(m2b2_bg.obj, 1, {
+    width: "0%"
+  }, {
+    delay: 1.5,
+    ease: Power1.easeInOut,
+    width: "43%"
+  });
+  TweenMax.fromTo(m2b3_bg.obj, 1, {
+    width: "0%"
+  }, {
+    delay: 2,
+    ease: Power1.easeInOut,
+    width: "100%"
+  });
+  var game = {
+    score1: 0,
+    score2: 0,
+    score3: 0
+  };
+  TweenMax.to(game, 1, {
+    delay: 1,
+    score1: "+=22",
+    roundProps: "score1",
+    onUpdate: function onUpdate() {
+      document.getElementById("m2b1_value").innerText = game.score1 + "%";
+    },
+    ease: Power1.easeInOut
+  });
+  TweenMax.to(game, 1, {
+    delay: 1.5,
+    score2: "+=31",
+    roundProps: "score2",
+    onUpdate: function onUpdate() {
+      document.getElementById("m2b2_value").innerText = game.score2 + "%";
+    },
+    ease: Power1.easeInOut
+  });
+  TweenMax.to(game, 1, {
+    delay: 2,
+    score3: "+=79",
+    roundProps: "score3",
+    onUpdate: function onUpdate() {
+      document.getElementById("m2b3_value").innerText = game.score3 + "%";
+    },
+    ease: Power1.easeInOut
+  });
+  var mod3 = new Sprite({
+    id: 'mod3',
+    class: 'mod3-style',
+    container: mod.obj
+  });
+  var mod3copy1 = new Sprite({
+    id: 'mod3copy1',
+    class: 'mod3copy1-style',
+    container: mod3.obj,
+    text: {
+      content: d.header3
+    }
+  });
+  var mod3graphics1 = new Sprite({
+    id: 'mod3graphics1',
+    class: 'mod3graphics1-style',
+    container: mod3.obj,
+    image: "assets/images/mod3graphix1@2x.png"
+  });
+  TweenMax.fromTo(mod3copy1.obj, 1, {
+    alpha: 0
+  }, {
+    delay: 1.5,
+    ease: Power1.easeInOut,
+    alpha: 1
+  });
+  TweenMax.fromTo(mod3graphics1.obj, 1, {
+    alpha: 0
+  }, {
+    delay: 2,
+    ease: Power1.easeInOut,
+    alpha: 1
+  });
+  console.log('version 1');
 }
 
 function startInfographic(d) {
@@ -4126,4 +4117,4 @@ function loadInfographic(d) {
   initStage(d.container); // initInfoAnimation(d.data);
 }
 
-//# sourceMappingURL=infographic-source.js.map
+//# sourceMappingURL=app-min.js.map
